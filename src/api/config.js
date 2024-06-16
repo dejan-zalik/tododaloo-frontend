e5e9d033d3f07caf9a72d5169a27af20f6a2af3f
Dejan <dkilaz@gmail.com>
2024-06-16 11:42:01 +0200

    config update

--- src/api/config.js
+++ src/api/config.js
@@ -1,1 +1,5 @@
-export const API_URL = `http://localhost:8080`;
+export const API_URL =
+  process.env.NODE_ENV === 'development'
+    ? `http://localhost:8080`
+    : 'https://api.tododaloo.xyz';
+console.log(process.env.NODE_ENV);
